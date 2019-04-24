0b0cd79494c57efd5f919bcb8b29f0132ca27ac4
Ehud Adler <easports96@gmail.com>
2019-04-24 00:47:47 -0400

    first commit

---
+++ bot.py
@@ -0,0 +1,81 @@
+import os
+import requests
+from datetime import datetime
+from threading import Timer
+
+numbers_list_file = "numbers.txt"
+timer = None
+
+def generate_message_for_day(day):
+    bracha = "בָּרוּךְ אַתָּה ה' אֱלֹהֵינוּ מֶלֶךְ הָעוֹלָם, אֲשֶׁר קִדְּשָׁנוּ בְּמִצְוֹתָיו וְצִוָּנוּ עַל סְפִירַת הָעֹמֶר"
+    harachaman = "הָרַחֲמָן הוּא יַחֲזִיר לָנוּ עֲבוֹדַת בֵּית הַמִּקְדָּשׁ לִמְקוֹמָהּ, בִּמְהֵרָה בְיָמֵינוּ אָמֵן סֶלָה"
+
+    lines, message = None, None
+    filename = 'sefira.txt'
+
+    message = bracha
+    message = "{0}\n\n".format(message)
+
+    with open(filename) as f:
+        lines = f.readlines()
+        start = day + (day * 2)
+        end = start+1
+        message = "{0}{1}".format(message, lines[start])
+        message = "{0}\n".format(message)
+        message = '{0}{1}\n'.format(message, lines[end])
+
+
+    message = '{0}{1}\n'.format(message, harachaman)
+    return message
+
+def send_message_to(message, number):
+    from_whatsapp_number='whatsapp:+14155238886'
+    to_whatsapp_number='whatsapp:{0}'.format(number)
+
+    response = requests.post(
+        'https://api.twilio.com/2010-04-01/Accounts/{0}/Messages.json'.format(os.getenv('TWILIO_ACCOUNT_SID')), 
+        data={
+            'To':'{0}'.format(to_whatsapp_number), 
+            'From':'{0}'.format(from_whatsapp_number), 
+            'Body':message 
+        },
+        auth=(
+            '{0}'.format(os.getenv('TWILIO_ACCOUNT_SID')), 
+            '{0}'.format(os.getenv('TWILIO_AUTH_TOKEN'))
+        )
+    )
+
+def calculate_day():
+    start = datetime(2019, 4, 21, 20, 0, 0)
+    current = datetime.now()
+    return abs((start-current).days)
+
+def run():
+    message = generate_message_for_day(calculate_day())
+    with open(numbers_list_file) as f:
+        lines = f.readlines()
+        for line in lines:
+            send_message_to(message, line)
+
+
+def update_timer():
+    x = datetime.today()
+
+    y = x.replace(
+        day= x.day+1,
+        hour=20, 
+        minute=0, 
+        second=0, 
+        microsecond=0
+    )
+
+    delta_t = y - x
+    secs = delta_t.seconds+1
+
+    timer = Timer(secs, run)
+    timer.start()
+
+def bootstrap():
+    update_timer()
+
+bootstrap()
---
+++ sefira.php
@@ -0,0 +1,5 @@
+<?php
+    $target_dir = "numbers.txt";
+    $target_file = dirname(__DIR__).'/'.$target_file;
+    echo ''.$_POST["email"].' >> '.$target_file
+?>
